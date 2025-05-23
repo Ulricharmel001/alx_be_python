0f893d099e6e4e50a30fd53893a9f627e123ddd6
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-05-23 09:56:24 +0100

    add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+# Baic operation
+number1 = 10
+number2 = 5
+sum = number1 + number2
+different = number1 - number2
+product = number1 * number2
+print('Addition of 10 and 5 is', sum)
+print('Subtraction of 10 and 5 is', different)
+print('Multiplication of 10 and 5 is', product)
