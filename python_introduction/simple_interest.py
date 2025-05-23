5a371938937a418d121b9f86afc62b056ed27977
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-05-23 11:43:26 +0100

    Add simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+"""
+#Writing python  script to compute simple interest 
+# Assigning value to variable 
+# Declaring formular
+#Printing output 
+"""
+principal = 1000 # initial investment
+rate = 0.05  # This is 5%  interest on investment
+time = 3 # 3 year , duration of investment
+interest = principal * rate * time # formular to compute interest
+print("The simple interest is:", interest) # display the interest amount
