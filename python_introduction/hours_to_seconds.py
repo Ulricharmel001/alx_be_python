ac94591c0a9ba9ba83f4987c691cf8f6f1699e8c
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-05-23 13:36:53 +0100

    updating changes i hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -3,7 +3,9 @@
 #Demining the formular to convert the hour to second 
 #Displaying the out of the converted value 
 
-hour = 2 # assumming an hour of 2
-seconds_in_one_hour = 60 * 60 # calculating the amount of seconds in an hour
-seconds_in_two_hours = seconds_in_one_hour * 2 # calculating seconds in  2 hours
-print( hour, "hour(s) is",  seconds_in_two_hours, "seconds")
+hours = 2 # asigning  an hour of 2
+seconds = 60 * 60 # calculating the amount of seconds in an hour
+
+seconds_in_two_hours = seconds * 2 # calculating seconds in  2 hours
+
+print( hours, "hour(s) is",  seconds_in_two_hours, "seconds")
