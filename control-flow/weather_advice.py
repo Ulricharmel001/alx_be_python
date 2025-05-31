57bbed2745ce08e3e43b859f471d6e09d22c1459
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-05-31 23:30:35 +0100

    making changes in weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,38 +1,7 @@
-# """
-# PSEUDOCODE
-#  *Program to execute base on user input
-#  Ask user current weather condition
-#  From the weather condition, the user provided, we recommend what to do
-#  would provide clothing recommendation base on the weather conditions
 
-
-#  *
-
-# """
-# """
-# algorithm 
-
-
-# prompt the user to enter the current weather condition
-# from the weather condition giving deduce the the type of dress the user should wear
-# there are just three weather condition in this  case sunny , rainy and cold
-# if the weather condition is sunny 
-#    wear a t-shirt and sunglasses 
-# if the weather condition is rainy
-#    don't forget your umbrella and rain boat
-# if the weather condition is cold
-#    Make sure you wear a warm coat a scart
-# else
-#  Sorry, I don't have recommendations for this weather
- 
-#  display the weather condition
-#  display the recommended clothing
-
-# """
 weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
-# print(user_current_weather_condition)
 if weather == "sunny":
-    print(" Wear a t-shirt and sunglasses.\n ")
+    print(" Wear a t-shirt and sunglasses. ")
 elif weather == "rainy":
     print("don't forget your umbrella and raincoat\n")
 elif weather == "cold":
