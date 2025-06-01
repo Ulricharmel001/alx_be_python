3d71a15780bab16794d57fb9d1dc3aa5479e113d
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-06-01 08:35:25 +0100

    Add multiplicaton_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input("Enter a number to see its multiplication table:"))
-for number in [1,2,3,4,5,6,7,8,9,10]:
-    result = number * number
-    print(f"{number} * {number} = {result}")
+for s in (1,2,3,4,5,6,7,8,9,10):
+    product = number * s
+    print(f"{number} * {s} = {product}")
