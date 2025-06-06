021293143ee52bbd1170787a195a55328b47a1f0
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-06-06 14:56:22 +0100

    add fns_and_dsa/arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,18 @@
 
+"""
+Creating a function to carryout basic maths operations
+parameters are "num1, num2 and operations"
+operation is done between num1 and num2
+value stored in a variable called result
+and the return value is the result of the operation
+division by 0 is not allowed
+if someone tries to divide by 0, it would display an "Error: cannot divide by 0"
 
-def perform_operation(num1, num2, operation):
+"""
+def perform_operation(num1, num2, operation): # function to carry out math operations
     if operation == "add":
         result = num1 + num2
-        return result
+        return result # return value
     elif operation == "subtract":
         result = num1 - num2
         return result
@@ -12,7 +21,7 @@ def perform_operation(num1, num2, operation):
         return result
     elif operation == "divide":
         if num2 == 0:
-            return "Error: Cannot divide by zero"
+            return "Error: Cannot divide by zero" # division by 0 is not possible
         else:
             result = num1 / num2
             return result
