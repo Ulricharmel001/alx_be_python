b00d82cb52e76b91ecae8cf22741256a6ff080b1
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-06-06 13:40:52 +0100

    add fns_and_dsa/arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -13,8 +13,9 @@ def perform_operation(num1, num2, operation):
             return result
         case "divide":
             if num2  == 0:
-                return "cannot divide by zero"
-                print("Cannot divide by zero.")
+                result = "Cannot divide by zero."
+                return result
+
             else:
                 result = num1 / num2
                 return result
