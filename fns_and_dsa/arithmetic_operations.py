dec249d9bb352fff708e62ffa689bf4029cfa61f
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-06-06 14:40:38 +0100

    add fns_and_dsa/arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,23 +1,21 @@
 
 
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            result = num1 + num2
-            return result
-        case "multiply":
-            result = num1 * num2
-            return result
-        case "subtract":
-            result = num1 - num2
-            return result
-        case "divide":
-            if num2  == 0:
-                result = "Cannot divide by zero."
-                return result
+    if operation == "add":
+        result = num1 + num2
+        return result
+    elif operation == "subtract":
+        result = num1 - num2
+        return result
+    elif operation == "multiply":
+        result = num1 * num2
+        return result
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Cannot divide by zero"
+        else:
             result = num1 / num2
             return result
-
 
 
 
